fcfa12c02f60f029d23fd0ae0c1f25ed9e761857
Thuy Le <thuy.t.le@tuni.fi>
2022-11-26 00:54:51 +0200

    Round 11 Mocha grader fix auth

--- controllers/users.js
+++ controllers/users.js
@@ -9,7 +9,7 @@ const roles = ["customer", "admin"];
  * @param {string} email The email to validate.
  * @returns {boolean} True if the email match the requirement, false otherwise.
  */
-const validateEmail = (email) => {
+ const validateEmail = (email) => {
   return String(email)
     .toLowerCase()
     .match(
@@ -24,14 +24,12 @@ const validateEmail = (email) => {
  * @returns {boolean} True if the user match the structure and exist in the database, false otherwise.
  */
 const validateUser = (user) => {
-  const pwdRegex =
-    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
   const errors = [];
 
-  if (!validateEmail(user["email"])) errors.push("Invalid email");
+  if (!validateEmail(user.email)) errors.push("Invalid email");
   if (user.email === undefined) errors.push("Missing email");
   if (user.name === undefined) errors.push("Missing name");
-  if (user["password"] === undefined) {
+  if (user.password === undefined) {
     errors.push("Missing password");
   }
   if (String(user.password).length < 10) errors.push("Password too short");
@@ -42,14 +40,13 @@ const validateUser = (user) => {
 /**
  * Send all users as JSON
  *
- * @param {http.ServerResponse} response The response to get all users from.
+ * @param {http.ServerResponse} response
  * @returns {http.ServerResponse} The response contains all users in JSON format.
  */
-const getAllUsers = async (response) => {
-  const allUsers = await User.find({});
-
+const getAllUsers = async response => {
+  const users = await User.find({});
   try {
-    return responseUtils.sendJson(response, allUsers);
+    return responseUtils.sendJson(response, users);
   } catch (err) {
     console.log(err);
   }
@@ -58,50 +55,49 @@ const getAllUsers = async (response) => {
 /**
  * Delete user and send deleted user as JSON
  *
- * @param {http.ServerResponse} response The response to execute the deletion
- * @param {string} userId User indentifier for finding user in the database
- * @param {object} currentUser (mongoose document object)
- * @returns {http.ServerResponse} response with content of deleted user
+ * @param {http.ServerResponse} response
+ * @param {string} userId
+ * @param {Object} currentUser (mongoose document object)
  */
-const deleteUser = async (response, userId, currentUser) => {
+const deleteUser = async(response, userId, currentUser) => {
   const userToDelete = await User.findById(userId).exec();
   if (userToDelete === null) {
     return responseUtils.notFound(response);
   }
-  if (String(userId) === String(currentUser["_id"])) {
+  if (String(userId) === String(currentUser._id)) {
     return responseUtils.badRequest(response, "Bad Request");
   }
-  await User.deleteOne({ _id: userId }).exec();
+  await User.deleteOne({ _id: userId });
   return responseUtils.sendJson(response, userToDelete);
 };
 
 /**
  * Update user and send updated user as JSON
  *
- * @param {http.ServerResponse} response response to edit and return back with updated user information
- * @param {string} userId User indentifier for finding user in the database
- * @param {object} currentUser (mongoose document object)
- * @param {object} userData JSON data from request body
- * @returns {http.ServerResponse} response with content of the updated user
+ * @param {http.ServerResponse} response
+ * @param {string} userId
+ * @param {Object} currentUser (mongoose document object)
+ * @param {Object} userData JSON data from request body
  */
-const updateUser = async (response, userId, currentUser, userData) => {
-  const userToUpdate = await User.findById(userId);
+const updateUser = async(response, userId, currentUser, userData) => {
+  const userToUpdate = await User.findById(userId).exec();
   if (userToUpdate === null) {
     return responseUtils.notFound(response);
   }
-  if (String(userId) === String(currentUser["_id"])) {
-    return responseUtils.badRequest(
-      response,
-      "Updating own data is not allowed"
-    );
+
+  if (String(userId) === String(currentUser._id)) {
+    return responseUtils.badRequest(response, "Updating own data is not allowed");
   }
-  if (userData["role"] === undefined) {
+
+  if (userData.role === undefined) {
     return responseUtils.badRequest(response, "Bad Request");
   }
-  if (!roles.includes(userData["role"])) {
+
+  if (!roles.includes(userData.role) || userData.role === undefined) {
     return responseUtils.badRequest(response, "Bad Request");
   }
-  userToUpdate["role"] = userData["role"];
+
+  userToUpdate.role = userData.role;
   await userToUpdate.save();
   return responseUtils.sendJson(response, userToUpdate);
 };
@@ -109,47 +105,42 @@ const updateUser = async (response, userId, currentUser, userData) => {
 /**
  * Send user data as JSON
  *
- * @param {http.ServerResponse} response response to edit and return back with given user information
- * @param {string} userId User indentifier for finding user in the database
- * @param {object} currentUser (mongoose document object)
- * @returns {http.ServerResponse} response with content of the given user.
+ * @param {http.ServerResponse} response
+ * @param {string} userId
+ * @param {Object} currentUser (mongoose document object)
  */
-const viewUser = async (response, userId, currentUser) => {
-  const userToFind = await User.findById(userId).exec();
-  if (userToFind === null) {
+const viewUser = async(response, userId, currentUser) => {
+  const user = await User.findById(userId).exec();
+  if (user === null) {
     return responseUtils.notFound(response);
   }
-  return responseUtils.sendJson(response, userToFind);
+
+  return responseUtils.sendJson(response, user);
 };
 
 /**
  * Register new user and send created user back as JSON
  *
- * @param {http.ServerResponse} response response to edit and return back with new/registered user information
- * @param {object} userData JSON data from request body
- * @returns {http.ServerResponse} response with content of the registered user.
+ * @param {http.ServerResponse} response
+ * @param {Object} userData JSON data from request body
  */
-const registerUser = async (response, userData) => {
-  const user = await User.findOne({ email: userData.email }).exec();
+const registerUser = async(response, userData) => {
   const errors = validateUser(userData);
-
-  if (errors.length > 0) {
-    return responseUtils.badRequest(response, "Bad Request");
+  const validEmail = await User.findOne({email: userData.email}).exec();
+  if (errors.length !== 0) {
+    return responseUtils.badRequest(response, 'Missing information');
   }
-  if (user !== null) {
-    return responseUtils.badRequest(response, "Bad Request");
+  
+  if (validEmail) {
+    return responseUtils.badRequest(response, 'Email is already in use');
   }
+  
+  // Create a new user
   const newUser = new User(userData);
-  newUser["role"] = "customer";
+  newUser.role = "customer";
   await newUser.save();
 
   return responseUtils.createdResource(response, newUser);
 };
 
-module.exports = {
-  getAllUsers,
-  registerUser,
-  deleteUser,
-  viewUser,
-  updateUser,
-};
+module.exports = { getAllUsers, registerUser, deleteUser, viewUser, updateUser };
