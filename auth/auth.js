6195664d37a580fc948093c644d60155d8fe3873
Thuy Le <thuy.t.le@tuni.fi>
2022-11-26 02:12:36 +0200

    Round 11 Mocha grader Change auth

--- auth/auth.js
+++ auth/auth.js
@@ -1,46 +1,41 @@
-const {getCredentials} = require('../utils/requestUtils');
-const User = require('../models/user');
+const { getCredentials } = require("../utils/requestUtils");
 
+const User = require("../models/user");
 
+const http = require("http");
 /**
  * Get current user based on the request headers
  *
- * @param {http.IncomingMessage} request
- * @returns {Object|null} current authenticated user or null if not yet authenticated
+ * @param {http.IncomingMessage} request The incoming user information authentication.
+ * @returns {object|null} current authenticated user or null if not yet authenticated
  */
-const getCurrentUser = async request => {
-  // Implement getting current user based on the "Authorization" request header
-  const authheader = request.headers.authorization;
-  // Return null if Authorization header is missing/empty
-  if (authheader === undefined | authheader === "") {
+const getCurrentUser = async (request) => {
+  // Check for Authorization header
+  const authHeader = request.headers["authorization"];
+  // Return null if Authorization header is missing/empty, or if Authorization type is not "Basic"
+  if (authHeader === undefined || authHeader === "") {
     return null;
-    }
-
+  }
   // Return null if Authorization header is not Basic
-  if (authheader.split(" ")[0] !== "Basic") {
+  const authType = authHeader.split(" ")[0];
+  if (authType !== "Basic") {
     return null;
   }
-
-  // Get credentials
-  const info = getCredentials(request);
-  const email = info[0];
-  const password = info[1];
-  if (info.length === 0) {
+  const credentials = getCredentials(request);
+  const userEmail = credentials[0];
+  const userPassword = credentials[1];
+  if (credentials.length === 0) {
     return null;
   }
-
-  // Check email
-  const currentUser = await User.findOne({email: email}).exec();
+  // const currentUser = await getUser(credentials[0], credentials[1]);
+  const currentUser = await User.findOne({ email: userEmail }).exec();
   if (currentUser === null) {
     return null;
   }
-
-  // Check password
-  const isPasswordCorrect = await currentUser.checkPassword(password);
+  const isPasswordCorrect = await currentUser.checkPassword(userPassword);
   if (!isPasswordCorrect) {
     return null;
   }
-
   return currentUser;
 };
 
